72c4a46330c6829290ec6ba380e8b85f6e797016
dyseyha68-maker <dyseyha68@gmail.com>
2026-04-02 21:26:31 +0700

    Fix static files for production

--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static('/static/', document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
