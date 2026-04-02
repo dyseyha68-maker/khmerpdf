ca05aafa3640541b3100b2cc9c3172095bb15f9f
dyseyha68-maker <dyseyha68@gmail.com>
2026-04-02 22:33:21 +0700

    Fix media files for production

--- config/settings.py
+++ config/settings.py
@@ -102,10 +102,17 @@ REST_FRAMEWORK = {
 
 # Static and Media files
 import os
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
