97037a3d165a6de0dc3b97ced27ccd906018d8a8
dyseyha68-maker <dyseyha68@gmail.com>
2026-04-02 23:00:12 +0700

    Fix media file serving in production

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
+from django.views.static import serve
+from django.urls import re_path
 from apps.pdf.views import index, split_page, merge_page, compress_page, organize_page, calendar_page, khqr
 
 urlpatterns = [
@@ -17,5 +18,10 @@ urlpatterns = [
     path('khqr/', khqr, name='khqr'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in production
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.DEBUG else settings.STATIC_ROOT)
