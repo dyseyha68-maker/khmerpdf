9b828aa98ba5f4fd5ca4e0fb06bd92e8387deb89
dyseyha68-maker <dyseyha68@gmail.com>
2026-04-02 22:45:42 +0700

    Fix media URL configuration

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from apps.pdf.views import index, split_page, merge_page, compress_page, organize_page, calendar_page, khqr
 
 urlpatterns = [
@@ -16,9 +17,5 @@ urlpatterns = [
     path('khqr/', khqr, name='khqr'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.DEBUG else settings.STATIC_ROOT)
